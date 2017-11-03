f9a0b49e438232c971815ec1ec6d29560d18ca35
Kien Nguyen <kiennn@viosoft.com>
2017-11-03 12:31:30 +0700

    Update

---
+++ C_file/convert.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int deTohex();
+int hexTode();
+int biTode();
+int deTobi();
+int biTohex();
+int hexTobi();
+
+int main() {
+  return 0;
+}
+
+int deTohex() {
+  return 0;
+}
+
+int hexTode() {
+  return 0;
+}
+
+int biTode() {
+  return 0;
+}
+
+int deTobi() {
+  return 0;
+}
+
+int biTohex() {
+  return 0;
+}
+
+int hexTobi() {
+  return 0;
+}
