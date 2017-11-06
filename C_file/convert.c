6732a6c0b5563a1784460b08034b3969fc8349df
kien nguyen <nguyenngockien29@gmail.com>
2017-11-06 17:46:47 +0700

    Convert decima to hexa

--- C_file/convert.c
+++ C_file/convert.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int deTohex();
+void deTohex(long decimaNum);
 int hexTode();
 int biTode();
 int deTobi();
@@ -9,11 +9,36 @@ int biTohex();
 int hexTobi();
 
 int main() {
-  return 0;
+	long a;
+	printf("Enter decima number\n");
+	scanf("%ld",&a);
+	deTohex(a);
+	return 0;
 }
 
-int deTohex() {
-  return 0;
+void deTohex(long decimaNum) {
+  long remainder,quotient,temp;
+  char hexadecimalNumber[100];
+  int i,j=0;
+  quotient = decimaNum;
+
+  while(quotient != 0) {
+  	temp = quotient % 16;
+  	if (temp < 10)
+  	{
+  		hexadecimalNumber[j++] = temp + 48;
+  		//temp = temp + 48;
+  	} else {
+  		hexadecimalNumber[j++] = temp + 55;
+  		//temp = temp + 55;
+  	}
+  	//hexadecimalNumber[i++] = temp;
+  	quotient = quotient/16;
+  	
+  }
+  for (i = j ;i>= 0;i--)
+	    printf("%c",hexadecimalNumber[i]);
+  printf("\n");
 }
 
 int hexTode() {
