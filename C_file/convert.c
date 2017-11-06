05f44a2ea53475230141bc0aee21ad26b93e8db5
Your Name <you@example.com>
2017-11-07 00:25:48 +0700

    Update convert

--- C_file/convert.c
+++ C_file/convert.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <math.h>
+#include  <string.h>
+#include <stdlib.h>
 
 void deTohex(long decimaNum);
-int hexTode();
-int biTode();
-int deTobi();
+void hexTode(char *hexaNum, int len);
+void biTode(char *biNum, int len);
+void deTobi(long decimaNum);
 int biTohex();
 int hexTobi();
 
 int main() {
 	long a;
+  char hex[100];
 	printf("Enter decima number\n");
 	scanf("%ld",&a);
 	deTohex(a);
+  deTobi(a);
+  printf("Enter hexxa number\n");
+  scanf("%s", hex);
+  int lenHex = strlen(hex);
+  hexTode(hex,lenHex);
+
+  char binaryNum[100];
+  printf("Enter binary number\n" );
+  scanf("%s",binaryNum);
+  int lenBi = strlen(binaryNum);
+  biTode(binaryNum,lenBi);
 	return 0;
 }
 
@@ -41,16 +55,49 @@ void deTohex(long decimaNum) {
   printf("\n");
 }
 
-int hexTode() {
-  return 0;
+void hexTode(char *hexaNum, int len) {
+  int i = 0,val;
+  len--;
+  long long decima =0;
+  for(i=0;hexaNum[i]!='\0';i++) {
+    if(hexaNum[i]>='0' && hexaNum[i]<='9') {
+      val = hexaNum[i] - 48;
+    } else if(hexaNum[i] >= 'a' && hexaNum[i] <='f') {
+      val = hexaNum[i] - 97 + 10;
+    } else if(hexaNum[i] >='A' && hexaNum[i] <='F') {
+      val = hexaNum[i] -65 + 10;
+    }
+    decima += val * pow(16,len);
+    len--;
+  }
+  printf("decima number = %lld\n",decima );
 }
 
-int biTode() {
-  return 0;
-}
+void biTode(char *biNum, int len) {
+  int i =0;
+  long decima =0;
+  printf("%d\n",len);
+  for(i=len-1;i>=0;i--) {
+    decima += (biNum[i] - 48) * (int)pow(2,len-1-i);
+    }
+    printf("decima number = %ld\n",decima);
+  }
 
-int deTobi() {
-  return 0;
+
+void deTobi(long decimaNum) {
+  long remainder,quotient,temp;
+  char binaryNumber[100];
+  int i,j=0;
+  quotient = decimaNum;
+  while(quotient !=0) {
+    temp = quotient % 2;
+    binaryNumber[j++] = temp +48;
+    quotient = quotient/2;
+  }
+  printf("binary number\n");
+  for (i = j ;i>= 0;i--)
+      printf("%c",binaryNumber[i]);
+  printf("\n");
 }
 
 int biTohex() {
